96e829ab51d861b919b9d3b01068adead4cb1e6d
Shai Guendelman <shaigue@csf.technion.ac.il>
2021-03-20 13:43:02 +0200

    updated main to run training

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 """Main script to run on the server"""
-# import config
-# from mathqa_processing import MathQAManager
-# from teacher_forcing_gru_encoder_decoder import Seq2Seq
-# from train_seq2seq_teacher_forcing import train, evaluate
+import config
+from mathqa_processing import MathQAManager
+from teacher_forcing_gru_encoder_decoder import Seq2Seq
+from train_seq2seq_teacher_forcing import train, evaluate
 
-
-def train_mathqa():
+def try_gpu():
     print("STARTING SCRIPT...")
     try:
         import torch
@@ -18,13 +17,16 @@ def train_mathqa():
     except:
         print("import torch FAIL")
     print("FINISHED")
-    # manager = MathQAManager(root_dir=config.MATHQA_DIR, max_vocabulary_size=config.MAX_VOCABULARY_SIZE, dummy=False)
-    # model = Seq2Seq(
-    #     source_vocabulary_size=manager.text_vocabulary_size,
-    #     target_vocabulary_size=manager.code_vocabulary_size,
-    #     hidden_dim=config.INTERNAL_DIM
-    # )
-    # train(model, manager, n_epochs=100)
+
+
+def train_mathqa():
+    manager = MathQAManager(root_dir=config.MATHQA_DIR, max_vocabulary_size=config.MAX_VOCABULARY_SIZE, dummy=False)
+    model = Seq2Seq(
+        source_vocabulary_size=manager.text_vocabulary_size,
+        target_vocabulary_size=manager.code_vocabulary_size,
+        hidden_dim=config.INTERNAL_DIM
+    )
+    train(model, manager, n_epochs=100)
 
 
 if __name__ == "__main__":
