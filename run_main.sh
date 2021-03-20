3d46361eac698d9b4044a043a9a60a947a4fb9f0
Shai Guendelman <shaigue@csf.technion.ac.il>
2021-03-20 12:15:36 +0200

    running scripts ready

--- run_main.sh
+++ run_main.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 # This is a script to run the main.py script after getting to the HPC server through sbatch
 python main.py
--- send_task.sh
+++ send_task.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # script that runs main python script for sbatch
 #https://slurm.schedmd.com/sbatch.html
 #https://hpc.cswp.cs.technion.ac.il/newton-computational-cluster/#using-slurm
