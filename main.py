53f647795eda1bf13546da9cc316dfa4f396b866
Shai Guendelman <shaigue@csf.technion.ac.il>
2021-04-17 10:41:37 +0300

    pass

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import macro_experiments
 
 
 if __name__ == "__main__":
-    # macro_experiments.high_complexity_macro()
+    macro_experiments.high_complexity_macro()
     macro_experiments.different_avg_len_macros(0)
     macro_experiments.different_avg_len_macros(1)
