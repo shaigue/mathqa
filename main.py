8509ca86eccf4960818d0551b89e62e4b7908ed6
Shai Guendelman <shaigue@csf.technion.ac.il>
2021-05-13 17:01:35 +0300

    passed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """Main script to run on the server"""
 import experiments
 import macro_experiments
-
+from train.mathqa_graph_generation_train import train
 
 if __name__ == "__main__":
+    train()
     # macro_experiments.high_complexity_macro()
-    macro_experiments.different_avg_len_macros(0)
-    macro_experiments.different_avg_len_macros(1)
+    # macro_experiments.different_avg_len_macros(0)
+    # macro_experiments.different_avg_len_macros(1)
